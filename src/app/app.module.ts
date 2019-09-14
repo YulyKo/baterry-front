2f6787b928157e6e3cac8b3e47cf8d94572f5ced
YulyKo <juliakotenko.forstudy@gmail.com>
2019-09-14 15:55:52 +0300

    create form for check cathegory of device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { ReactiveFormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { CardForAddComponent } from './elements/card-for-add/card-for-add.component';
 import { CardOfAddedComponent } from './elements/card-of-added/card-of-added.component';
 import { DayThemeComponent } from './pages/home-page/day-theme/day-theme.component';
 import { NightThemeComponent } from './pages/home-page/night-theme/night-theme.component';
-import { FormForChoosingComponent } from './pages/form-for-choosing/form-for-choosing.component';
 import { ForChoosingPageComponent } from './pages/for-choosing-page/for-choosing-page.component';
 import { ModalFormComponent } from './elements/modal-form/modal-form.component';
 import { NotFoudPageComponent } from './pages/not-foud-page/not-foud-page.component';
@@ -21,14 +22,15 @@ import { NotFoudPageComponent } from './pages/not-foud-page/not-foud-page.compon
     CardOfAddedComponent,
     DayThemeComponent,
     NightThemeComponent,
-    FormForChoosingComponent,
     ForChoosingPageComponent,
     ModalFormComponent,
     NotFoudPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
